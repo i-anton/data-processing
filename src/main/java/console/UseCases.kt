554aa3b728bc14e4e852f471f646621ccca8ef16
i-anton <ia6145@gmail.com>
2019-10-22 23:16:14 +0300

    added anti shift, anti spike, anti trend, dft

--- src/main/java/console/UseCases.kt
+++ src/main/java/console/UseCases.kt
@@ -6,6 +6,11 @@ import data.analysis.LineStatistics
 import data.input.LineGenerator
 import data.model.Combine
 import data.model.Combine.multiNoiseAvg
+import data.model.Filter.antiShift
+import data.model.Filter.antiSpike
+import data.model.Filter.antiTrend
+import data.model.Filter.trendDetect
+import data.model.SingleTransforms
 import de.gsi.dataset.spi.DoubleDataSet
 import gui.ShowCase
 import javafx.application.Platform
@@ -86,5 +91,63 @@ fun dftDemo() {
     }
     Platform.startup {
         ShowCase().multi(dataList).show()
+    }
+}
+
+fun removeConstantShiftDemo() {
+    val initialRand = LineGenerator.random(100, -1.0, 1.0)
+    val shiftedRand = SingleTransforms.shift(Line(initialRand), -100.0,
+            initialRand.size.toDouble() + 1,
+            100.0, 1.0)
+
+    val fixed = antiShift(shiftedRand)
+
+    Platform.startup {
+        ShowCase().multi(listOf(
+                listOf(DoubleDataSet("line").addLine(initialRand)),
+                listOf(DoubleDataSet("shifted").addLine(shiftedRand)),
+                listOf(DoubleDataSet("f(random)").addLine(fixed)),
+                listOf(DoubleDataSet("random").addLine(fixed))
+        )).show()
+    }
+}
+
+fun removeSpikesDemo() {
+    val initialRand = LineGenerator.random(100, -1.0, 1.0)
+    val spikedRand = SingleTransforms.spikes(Line(initialRand), LineGenerator.anySeed(),
+            10,
+            5.0)
+
+    val fixed = antiSpike(spikedRand,1.0)
+
+    Platform.startup {
+        ShowCase().multi(listOf(
+                listOf(DoubleDataSet("line").addLine(initialRand)),
+                listOf(DoubleDataSet("shifted").addLine(spikedRand)),
+                listOf(DoubleDataSet("f(random)").addLine(fixed)),
+                listOf(DoubleDataSet("random").addLine(fixed))
+        )).show()
+    }
+}
+
+fun trendDetectionDemo() {
+    val dots = 100
+    val initialRand = LineGenerator.random(dots, -1.0, 1.0)
+    val spikedRand = SingleTransforms.spikes(Line(initialRand), LineGenerator.anySeed(),
+            10,
+            5.0)
+    val trendLine = LineGenerator.linear(dots, 0.75, 50.0)
+    val trendy = Combine.additive(spikedRand, trendLine)
+
+    val windowSize = 3
+    val antiTrend = antiTrend(trendy, windowSize)
+    val detectedTrend = Line(trendDetect(trendy, windowSize))
+    Platform.startup {
+        ShowCase().multi(listOf(
+                listOf(DoubleDataSet("line").addLine(spikedRand)),
+                listOf(DoubleDataSet("trendy").addLine(trendy)),
+                listOf(DoubleDataSet("antiTrend").addLine(antiTrend)),
+                listOf(DoubleDataSet("detectedTrend").addLine(detectedTrend))
+        )).show()
     }
 }
